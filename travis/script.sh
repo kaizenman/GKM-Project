209226f915cc03a4085d3a05c19abc8022ea3e90
kaizenman <makolyan2@ukr.net>
2019-05-28 22:16:34 +0300

    ninja

--- travis/script.sh
+++ travis/script.sh
@@ -2,7 +2,11 @@
 
 source travis/vars.sh
 
-apt-get -y install ninja-build
+# ninja
+wget https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-linux.zip
+unzip ninja-linux.zip -d /usr/local/bin/
+update-alternatives --install /usr/bin/ninja ninja /usr/local/bin/ninja 1 --force
+
 which ninja
 echo $PATH
 
