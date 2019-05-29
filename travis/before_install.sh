858f87cfe824e1b7be0e4a684532eb1da86bb56d
kaizenman <makolyan2@ukr.net>
2019-05-29 22:50:47 +0300

    compilation time

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -31,45 +31,45 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     yes | sdkmanager --licenses > /dev/null 2>&1
     sdkmanager "ndk-bundle" > /dev/null 2>&1
     apt-get -y install cmake
-#    apt-get -y install ninja-build
+    apt-get -y install ninja-build
     apt-get -y install software-properties-common > /dev/null 2>&1
-#    wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
-#    apt-get update
-#    apt-add-repository "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-6.0 main"
-#    apt-get install -y clang-6.0 lld-6.0
+    wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
+    apt-get update
+    apt-add-repository "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-6.0 main"
+    apt-get install -y clang-6.0 lld-6.0
 
     #apt-get dist-upgrade -y > /dev/null 2>&1
     #echo "deb http://ftp.us.debian.org/debian jessie main contrib non-free" >> /etc/apt/sources.list.d/jessie.list
     #echo "deb http://ftp.us.debian.org/debian unstable main contrib non-free" >> /etc/apt/sources.list.d/unstable.list
-#    apt-get update > /dev/null 2>&1
-#    apt install python -y > /dev/null 2>&1
-#    apt install python3 -y > /dev/null 2>&1
-#    apt-get install -y g++-4.9 g++-8 > /dev/null 2>&1
-#    apt-get install -y clang++-6.0 > /dev/null 2>&1
-#    apt-get install libc++-dev -y > /dev/null 2>&1
-#    apt-get install libc++abi-dev -y > /dev/null 2>&1
-#    apt-get autoremove --purge -y > /dev/null 2>&1
-#    apt-get autoclean -y > /dev/null 2>&1
-#    rm -rf /var/cache/apt/* /tmp/* > /dev/null 2>&1
+    apt-get update > /dev/null 2>&1
+    apt install python -y > /dev/null 2>&1
+    apt install python3 -y > /dev/null 2>&1
+    apt-get install -y g++-4.9 g++-8 > /dev/null 2>&1
+    apt-get install -y clang++-6.0 > /dev/null 2>&1
+    apt-get install libc++-dev -y > /dev/null 2>&1
+    apt-get install libc++abi-dev -y > /dev/null 2>&1
+    apt-get autoremove --purge -y > /dev/null 2>&1
+    apt-get autoclean -y > /dev/null 2>&1
+    rm -rf /var/cache/apt/* /tmp/* > /dev/null 2>&1
     
     
-#    echo "Setting g++ 8 as default compiler"
-#    update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-8 1 > /dev/null 2>&1
-#    echo "Setting python3 as default"
-#    update-alternatives --install /usr/bin/python python /usr/bin/python3.7 1 > /dev/null 2>&1
-#    update-alternatives --install /usr/bin/python python /usr/bin/python2.7 2 > /dev/null 2>&1
+    echo "Setting g++ 8 as default compiler"
+    update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-8 1 > /dev/null 2>&1
+    echo "Setting python3 as default"
+    update-alternatives --install /usr/bin/python python /usr/bin/python3.7 1 > /dev/null 2>&1
+    update-alternatives --install /usr/bin/python python /usr/bin/python2.7 2 > /dev/null 2>&1
     #echo "Getting polly..."
     #wget --no-check-certificate -O v0.10.3.tar.gz https://github.com/ruslo/polly/archive/v0.10.3.tar.gz
     #tar xvzf v0.10.3.tar.gz
-#    echo "Setting environment variables"
-#    echo "... Adding /usr/bin/clang++-6.0/bin to PATH variable"
-#    echo "... Adding /usr/lib/clang/6.0/lib to LD_LIBRARY_PATH variable"
-#    ln -s /usr/bin/clang++-6.0 /usr/bin/clang++
-#    ln -s /usr/bin/clang-6.0 /usr/bin/clang
+    echo "Setting environment variables"
+    echo "... Adding /usr/bin/clang++-6.0/bin to PATH variable"
+    echo "... Adding /usr/lib/clang/6.0/lib to LD_LIBRARY_PATH variable"
+    ln -s /usr/bin/clang++-6.0 /usr/bin/clang++
+    ln -s /usr/bin/clang-6.0 /usr/bin/clang
     #echo "... Adding /polly-0.10.3 to POL\LY_ROOT variable"
     #export POLLY_ROOT=/polly-0.10.3
-#    echo "... Getting opengl libraries"
-#    echo "Installing RANDR!!!"
+    echo "... Getting opengl libraries"
+    echo "Installing RANDR!!!"
   #  apt-get update
     #git clone https://github.com/glfw/glfw
     #cd glfw
